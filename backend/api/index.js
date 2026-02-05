a438d5787badde102ffead5203e3f1cd75cfe638
Mathangi2003 <mathangik10@gmail.com>
2026-02-05 19:29:05 +0530

    updated first commit

---
+++ backend/api/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Backend running successfully 🚀");
+});
+
+// routes
+import authRoutes from "../routes/authRoutes.js";
+app.use("/api/auth", authRoutes);
+
+export default app;
